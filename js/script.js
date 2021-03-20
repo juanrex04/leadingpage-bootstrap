7db8468978f72c875995cdd11099060a4cc4e253
Juan Manuel <juanpix2404@gmail.com>
2021-03-20 14:14:20 -0500

    Se añade funcionalidad al form de contact us Se añade un toast para el form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Working!')
+console.log("Working!");
+
+const form = document.querySelector("#formContact");
+
+const send = document.querySelector("#btnSend");
+const sending = document.querySelector("#btnSending");
+
+const toast = document.querySelector(".toast");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  console.log("campo email", data.get("emailPerson"));
+  console.log("Nombre persona", data.get("namePerson"));
+  console.log("Comentarios ", data.get("textComments"));
+
+  send.classList.add("d-none");
+  sending.classList.remove("d-none");
+  window.setTimeout(() => {
+    send.classList.remove("d-none");
+    sending.classList.add("d-none");
+
+    const eventToast = new bootstrap.Toast(toast, 1000);
+
+    eventToast.show();
+  }, 3000);
+  form.reset();
+});
